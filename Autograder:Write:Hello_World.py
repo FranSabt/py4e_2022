4be55763080a156bcee7d99c8a4979d754681dc6
FranSabt <hsabatino1@gmail.com>
2022-03-06 14:47:46 -0400

    Chapter 2 files completed

---
+++ Autograder:Write:Hello_World.py
@@ -0,0 +1,6 @@
+# the code below almost works
+print("hello world") #write pinr(q) instead of prin(t)
+
+"""This is a little demonstration of the kind of mistakes you will encounter in the path to
+learning coding, not just with python but with any programming language that you encounter.
+Take your time a read carefully"""
