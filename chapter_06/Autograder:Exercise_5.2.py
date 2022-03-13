1f48deefe4cbadb2d0b8029ca8c844a72a2e8004
FranSabt <88568637+FranSabt@users.noreply.github.com>
2022-03-13 13:17:17 -0400

    Comments added

--- chapter_06/Autograder:Exercise_5.2.py
+++ chapter_06/Autograder:Exercise_5.2.py
@@ -1,17 +1,20 @@
-from cgitb import small
-
+# IMPORTANT!!! This code does not run well in Python3.8, I have a machine that I cannot configure for python 3.10, 
+# but in other machine or online interpreter works well.
 
 largest = None
 smallest = None
-while True:
-    num = input("Enter a number: ")
-    int(num)
-    if num == 'done': break
-    elif largest == None or num > largest: 
-        largest = num
-    elif smallest == None or num < smallest:
-        smallest = num
-    else: print('Invalid input')
+while True: # This means that the loop will always run as log the 'done' statement is not reached
+    try:
+        if num == 'done': break # This line has the objective to terminated the loop. 
+        num = int(num)
+        if largest == None or num > largest: # The variables has values "None", so, for the first number is necessary to
+            largest = num                    # assing some number, the next comparison will do the job.
+        elif smallest == None or num < smallest:
+            smallest = num
+        else : continue
+    except: 
+        print('Invalid input') # This line will run when the input is the input is no 'done' or a number,
+                               # also, if you put a float it will be comberted to int.
 
 print("Maximum", largest)
 print('Minimun', smallest)
