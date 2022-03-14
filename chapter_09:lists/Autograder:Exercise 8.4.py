abfa2d14a5b4a8aa04242a8a893546530d5bedc1
FranSabt <88568637+FranSabt@users.noreply.github.com>
2022-03-14 18:56:31 -0400

    Comments!

--- chapter_09:lists/Autograder:Exercise 8.4.py
+++ chapter_09:lists/Autograder:Exercise 8.4.py
@@ -6,5 +6,12 @@ for line in fh:
     for word in x:
         if word not in lst:
             lst.append(word)
-lst.sort()       
+            
+# You must be already familiarized with the code, if not, check the solution to the "Strings" chapter.
+# The only that we need is to check if the word is already in the list, if not it will appear, is not necessary to make an "else: continue",
+# because if the code doesn't execute the append statement it will continue to the next iteration.
+
+# It could be that in older versions of Python were necessary to make a continued statement, but I don-t know.
+
+lst.sort() # We need to put some order in the list.      
 print(lst)
