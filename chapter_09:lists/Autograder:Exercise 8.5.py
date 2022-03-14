0d7e334b1532d1168a21f84a6c1e4681f4907fc1
FranSabt <88568637+FranSabt@users.noreply.github.com>
2022-03-14 19:11:34 -0400

    Comments!

--- chapter_09:lists/Autograder:Exercise 8.5.py
+++ chapter_09:lists/Autograder:Exercise 8.5.py
@@ -7,15 +7,17 @@ if len(fname) < 1:
 fh = open(fname)
 for line in fh:
     if line.startswith('From:'):
-        count += 1
-        for mail in line.split():
-            if '@' in mail: 
+        for mail in line.split(): # I chose the name of the variable because it is what I looking for.
+            if '@' in mail: # If the string/data contains an "@"  it will print the data and make the count.
+                count += 1
                 print(mail)
 
 
 print("There were", count, "lines in the file with From as the first word")
 
-
+# So, the code below is how most people resolve the exercises, but I think it's not the best solution, 
+# is possible that for any reason the second data in the list is not the mail, in that case, your code will end up broken.
+# But is a code that gets the desired result, it's up to you to decide.
 
 """
 for line in fh :
