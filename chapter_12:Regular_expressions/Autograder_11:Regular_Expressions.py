bd296a2c0c2092b07e08e9937bd4693dccbbc6af
FranSabt <88568637+FranSabt@users.noreply.github.com>
2022-03-31 20:02:25 -0400

    Add files via upload

--- chapter_12:Regular_expressions/Autograder_11:Regular_Expressions.py
+++ chapter_12:Regular_expressions/Autograder_11:Regular_Expressions.py
@@ -9,15 +9,16 @@ print("file:",  fh)
 
 for line in fh:
     line2 = line.strip()
-    x = re.findall('\s*[0-9]+\s*', line2) # This regex will give us only estring of numbers
-                                          # "s*" will macth from a white space troug number 
-                                          # to another white space
-    if x != []: # All the lines that not contains number will be emtpy dictionaries
-                # so we wanted avoid these in order to obtein numbers.
+    x = re.findall('\s*[0-9]+\s*', line2) # This regex will give us only string of numbers.
+                                          # "\s*[0-9]+\s*" will match from whitespace through
+                                          #  number until find another whitespace.
+    if x != []: # All the lines that not contains number will be empty dictionaries
+                # so we wanted to avoid these in order to obtain numbers.
         for number in x: # "x" contains dictionaries, even if they have only one number, 
                          #so we need to traverse every one of the elements of the dictionary.
             total += int(number)  # And finally, we transform the string to int a make the sum.
-            value += 1 # Keep the count
+            value += 1 # Keep the count of numbers that we find.
 
 print(total, "", value)
 
+# Remember that the page generates individuals regex.txt for every user.
